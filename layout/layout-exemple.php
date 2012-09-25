de6720b46d76ee6fd71e99325ceb5210a9a80aa3
David Sferruzza <david.sferruzza@gmail.com>
2012-09-25 16:23:44 +0200

    Ajout de Twitter Bootstrap 2.1.1

--- layout/layout-exemple.php
+++ layout/layout-exemple.php
@@ -3,7 +3,9 @@
 	<head>
 		<title>Titre</title>
 		<meta charset="utf-8" />
-		<!--<link rel="stylesheet" href="" />-->
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/bootstrap-responsive.min.css" />
+		<script src="js/bootstrap.min.js"></script>
 	</head>
 	<body>
 		<?php /* Affichage de la page demandée */ if (isset($page)) include $page; ?>
