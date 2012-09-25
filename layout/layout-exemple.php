52e968b612db74680abba40fadb547c890b2ab2b
David Sferruzza <david.sferruzza@gmail.com>
2012-09-25 16:30:20 +0200

    Ajout de jQuery 1.8.2

--- layout/layout-exemple.php
+++ layout/layout-exemple.php
@@ -5,9 +5,12 @@
 		<meta charset="utf-8" />
 		<link rel="stylesheet" href="css/bootstrap.min.css" />
 		<link rel="stylesheet" href="css/bootstrap-responsive.min.css" />
+		<script src="js/jquery-1.8.2.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
 	</head>
 	<body>
-		<?php /* Affichage de la page demandée */ if (isset($page)) include $page; ?>
+		<div class="container">
+			<?php /* Affichage de la page demandée */ if (isset($page)) include $page; ?>
+		</div>
 	</body>
 </html>
